cf7124907dd08f44bc89a348ceb8abafb0f72d0a
thinkyside <eunlyuing@gmail.com>
2025-02-09 18:29:58 +0900

    [main] Package 파일 생성

---
+++ Qapple-Repository/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Qapple-Repository",
+    products: [
+        .library(
+            name: "Qapple-Repository",
+            targets: ["Qapple-Repository"]),
+    ],
+    targets: [
+        .target(
+            name: "Qapple-Repository"),
+        .testTarget(
+            name: "Qapple-RepositoryTests",
+            dependencies: ["Qapple-Repository"]
+        ),
+    ]
+)
