57b6ba0f62c5ed24a4c8aabee59559ec8360033d
thinkyside <eunlyuing@gmail.com>
2025-02-20 18:02:18 +0900

    [#13] TestHelper 싱글톤 로직 수정

--- Tests/QappleRepositoryTests/TestHelper+.swift
+++ Tests/QappleRepositoryTests/TestHelper+.swift
@@ -19,22 +19,18 @@ final class TestHelper {
     
     /// 테스트 토큰을 발급합니다.
     func testToken() async throws -> String {
-        if accessToken.isEmpty {
-            let localSignIn = try await MemberAPI.localSignIn(
-                testId: "TEST_ID_\(Date.now.description)",
-                deviceToken: "TEST_DEVICE_TOKEN",
-                server: .test
-            )
-            let localSignUp = try await MemberAPI.signUp(
-                signUpToken: localSignIn.refreshToken,
-                email: "email",
-                nickname: "nickname",
-                deviceToken: "deviceToken",
-                server: .test
-            )
-            return localSignUp.accessToken ?? ""
-        } else {
-            return accessToken
-        }
+        let localSignIn = try await MemberAPI.localSignIn(
+            testId: "TEST_ID_\(Date.now.description)",
+            deviceToken: "TEST_DEVICE_TOKEN",
+            server: .test
+        )
+        let localSignUp = try await MemberAPI.signUp(
+            signUpToken: localSignIn.refreshToken,
+            email: "email",
+            nickname: "nickname",
+            deviceToken: "deviceToken",
+            server: .test
+        )
+        return localSignUp.accessToken ?? ""
     }
 }
