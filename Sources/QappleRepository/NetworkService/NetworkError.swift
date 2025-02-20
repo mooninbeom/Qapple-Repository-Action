fb2a2581c20bdef2eeb12c49ab78ff3c95775408
thinkyside <eunlyuing@gmail.com>
2025-02-20 20:28:56 +0900

    [#13] AuthenticationFailed 네트워크 에러 정의

--- Sources/QappleRepository/NetworkService/NetworkError.swift
+++ Sources/QappleRepository/NetworkService/NetworkError.swift
@@ -8,13 +8,16 @@
 import Foundation
 
 /// 네트워킹에서 발생할 수 있는 에러를 정의합니다.
-enum NetworkError: Error {
+public enum NetworkError: Error {
     
     /// URLReuqest 함수 호출에 실패했습니다.
     case urlRequestFailure(urlString: String)
     
     /// 유효하지 않은 Response입니다.
     case invalidResponse(urlString: String, statusCode: Int, message: String)
+    
+    /// 인증에 실패했습니다.(403 에러)
+    case authenticationFailed
     
     /// Decoding에 실패했습니다.
     case decodingFailure(type: Decodable.Type)
